05a04f7a1daf9c2d4ce70cfb06330e15b7557f0a
yashvx <yashvdadhich04@gmail.com>
2026-06-11 15:14:58 +0530

    Day 1 - Band Name Generator

---
+++ Project 1 - Band Name Generator/project1.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in? ")
+pet = input("What's the name of your pet? ")
+
+print("Your Band name is: "+ city + " " + pet)
+
